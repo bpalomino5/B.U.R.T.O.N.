f192ac14d9a089ce8da4b075b0738ecfa22fe399
Brandon Palomino <12bpalomino@gmail.com>
2017-11-27 18:56:29 -0800

    adding pixels

---
+++ pixels/pixels.py
@@ -0,0 +1,69 @@
+
+import apa102
+import time
+import threading
+from gpiozero import LED
+try:
+    import queue as Queue
+except ImportError:
+    import Queue as Queue
+
+from alexa_led_pattern import AlexaLedPattern
+
+class Pixels:
+    PIXELS_N = 12
+
+    def __init__(self, pattern=AlexaLedPattern):
+        self.pattern = pattern(show=self.show)
+
+        self.dev = apa102.APA102(num_led=self.PIXELS_N)
+        
+        self.power = LED(5)
+        self.power.on()
+
+        self.queue = Queue.Queue()
+        self.thread = threading.Thread(target=self._run)
+        self.thread.daemon = True
+        self.thread.start()
+
+        self.last_direction = None
+
+    def wakeup(self, direction=0):
+        self.last_direction = direction
+        def f():
+            self.pattern.wakeup(direction)
+
+        self.put(f)
+
+    def listen(self):
+        if self.last_direction:
+            def f():
+                self.pattern.wakeup(self.last_direction)
+            self.put(f)
+        else:
+            self.put(self.pattern.listen)
+
+    def think(self):
+        self.put(self.pattern.think)
+
+    def speak(self):
+        self.put(self.pattern.speak)
+
+    def off(self):
+        self.put(self.pattern.off)
+
+    def put(self, func):
+        self.pattern.stop = True
+        self.queue.put(func)
+
+    def _run(self):
+        while True:
+            func = self.queue.get()
+            self.pattern.stop = False
+            func()
+
+    def show(self, data):
+        for i in range(self.PIXELS_N):
+            self.dev.set_pixel(i, int(data[4*i + 1]), int(data[4*i + 2]), int(data[4*i + 3]))
+
+        self.dev.show()
